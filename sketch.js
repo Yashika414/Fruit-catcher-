cc557c52c39fd6de4feea78f322eb4a6f917af77
Yashika414 <78353827+Yashika414@users.noreply.github.com>
2021-05-10 17:09:01 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,79 @@
+
+
+
+function setup() {
+  createCanvas(800,800);
+//  for (i = 1 ; i <= 10; i = i + 1)
+//  {
+//    console.log("yashika")
+//  }
+// for (i = 1 ; i <= 20 ; i = i + 1)
+// {
+//  console.log(i)
+// }
+// for (i = 2 ; i <= 20 ; i = i + 2)
+// {
+//   console.log(i)
+// }
+// for (i = 1 ; i <= 50 ; i = i + 1)
+// {
+//   if (i%5 == 0)
+//   {
+//    console.log(i)
+//   }
+// }
+// for (i = 50 ; i <= 60 ; i = i + 1 )
+// {
+
+//     console.log(i%10)
+//   }                                 
+
+// var a = 13
+// i = a % 10
+// j = Math.trunc(a / 10)
+// console.log(i+j)
+
+// for (i = 10 ; i <= 20 ; i = i + 1)
+// {
+ 
+//  c = i % 10 
+//  j = Math.trunc(i / 10)
+//  console.log(c+j)
+// }
+
+//var count = 0;
+// for (i = 1 ; i <= 30 ; i = i + 1)
+// {
+//  if (i%2 == 0)
+//  {
+//   count = count + 1
+// }
+// }
+// console.log(count)
+
+// var a = [10,20,30,40,50]
+// for (i  = 0 ; i <= 4 ; i = i+ 1)
+// {
+//  console.log(a [i])
+// }
+var a = [5,10,15,20,25,30]
+for (i = 0; i <= 5 ; i = i + 1)
+{
+  if (a[i] % 2 == 0)
+  {
+    console.log(a[i])
+  }
+}
+
+  
+}
+
+function draw() {
+  background("black");  
+ 
+  
+}
+
+
+
+
